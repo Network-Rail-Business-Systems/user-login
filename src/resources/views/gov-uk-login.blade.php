4ad283cf5359c2f4f0ae71c3b8991ebbb1b03a35
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-02-11 10:35:06 +0000

    Fixed banner colour

--- src/resources/views/gov-uk-login.blade.php
+++ src/resources/views/gov-uk-login.blade.php
@@ -1,5 +1,5 @@
 @section('heading')
-    <x-govuk::notification-banner title="Cookies" colour="blue">
+    <x-govuk::notification-banner title="Cookies" colour="info">
         <p class="govuk-body">We use some essential cookies to make this service work.</p>
         <p class="govuk-body">Your browser session will be stored once you sign in.</p>
     </x-govuk::notification-banner>
